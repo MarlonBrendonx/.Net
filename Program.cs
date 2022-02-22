2fbbb0df25bbe9047d994e22de44ad2540916225
Marlon Brendo <59180172+MarlonBrendonx@users.noreply.github.com>
2022-02-22 18:11:14 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+using static System.Console;
+using Projeto.src.Entities;
+
+public class Program{
+
+
+    public static void  Main(string[] args){
+
+       Knight arus = new Knight("Arus ",23,"Knight");
+       Wizzard wizzard = new Wizzard("wizzard ",23,"wizzard");
+
+       WriteLine(wizzard.Attack(1));
+       WriteLine(wizzard.Attack(7));
+      
+      
+
+    }
+
+}
